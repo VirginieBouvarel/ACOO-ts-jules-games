aa4df80d6609448f4fe416242eceb98e1cd8f069
Virginie <vb@ikimo9.com>
2024-10-04 11:26:51 +0200

    iteration-2

---
+++ annexes/key-features/2-unit/1-requirements.md
@@ -0,0 +1,8 @@
+# Caractéristique 2 : Des unités spécifiques à chaque jeu
+
+## Liste des exigences à implémenter
+
+(1) Chaque unité doit avoir des propriétés et les concepteurs de jeux peuvent ajouter de nouvelles proprietes 
+aux types d'unité dans leurs propres jeux.
+(2) Les unités doivent pouvoir se deplacer d'une case à une autre sur un plateau (ne sera pas implémentée DANS le système)
+(3) Les unités peuvent etre regroupées pour former des armées 
--- src/Unit.ts
+++ src/Unit.ts
@@ -1,3 +1,49 @@
+import { Weapon } from "./Weapon";
+
 export class Unit {
-  constructor() {}
+  private type: string | undefined;
+  private id: number;
+  private name: string | undefined;
+  private weapons: Weapon[] = [];
+  private properties: Map<string, any> = new Map();
+
+  constructor(id: number) {
+      this.id = id;
+  }
+
+  getId(): number {
+    return this.id;
+}
+
+  getType(): string | undefined {
+      return this.type;
+  }
+
+  setType(type: string): void {
+      this.type = type;
+  }
+
+  getName(): string | undefined {
+      return this.name;
+  }
+
+  setName(name: string): void {
+      this.name = name;
+  }
+
+  getWeapons(): Weapon[] {
+      return this.weapons;
+  }
+
+  addWeapon(weapon: Weapon): void {
+      this.weapons.push(weapon);
+  }
+
+  setProperty(property: string, value: any): void {
+      this.properties.set(property, value);
+  }
+
+  getProperty(property: string): any | undefined {
+      return this.properties.get(property);
+  }
 }
